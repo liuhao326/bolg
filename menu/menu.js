d297fd541d41a908e2c26784cd9e4c71b8c4b983
liuhao326 <326047736@qq.com>
2020-04-01 23:39:57 +0800

    完善注释

--- menu/menu.js
+++ menu/menu.js
@@ -9,7 +9,7 @@
 */
 var BlogDirectory = {
     /*
-        获取元素位置，距浏览器左边界的距离（left）和距浏览器上边界的距离（top）
+    获取元素位置，距浏览器左边界的距离（left）和距浏览器上边界的距离（top）
     */
     getElementPosition:function (ele) {        
         var topPosition = 0;
@@ -31,7 +31,7 @@ var BlogDirectory = {
     },
     
     /*
-    移动滚动条，finalPos 为目的位置，internal 为移动速度
+    点击目录标题后调用实现移动滚动条至目的地，finalPos 为目的位置，internal 为移动速度
     */
     moveScrollBar:function(finalpos, interval) {
 
@@ -61,9 +61,9 @@ var BlogDirectory = {
             return true; 
         } 
 		
-		/*未到达，则计算**下一步**所要移动的距离，其中除以10表明移动分十步完成*/
+		/*未到达预定位置，则计算**下一步**所要移动的距离，其中除以10表明移动分十步完成*/
         if (currentpos < finalpos) {
-			//Math.ceil() 函数返回大于或等于一个给定数字的最小整数。
+			//Math.ceil()函数返回大于或等于一个给定数字的最小整数。
             dist = Math.ceil((finalpos - currentpos)/15); 
             currentpos += dist; 
         } 
@@ -86,12 +86,16 @@ var BlogDirectory = {
         }
         
         /*如果没有到达锚点位置，则进行**下一步**移动*/
-        var repeat = "BlogDirectory.moveScrollBar(" + finalpos + "," + interval + ")"; 	//setTimeout()方法设置一个定时器，该定时器在定时器到期后执行一个函数或指定的一段代码。
+        var repeat = "BlogDirectory.moveScrollBar(" + finalpos + "," + interval + ")"; 	
+		//setTimeout()方法设置一个定时器，该定时器在定时器到期后执行一个函数或指定的一段代码。
 		//返回值timeoutID是一个正整数， 表示定时器的编号。这个值可以传递给clearTimeout()来取消该定时器。
 		//返回值赋值给当前文档中<body>元素或<frameset>元素的movement值
         document.body.movement = setTimeout(repeat, interval); 
     },
-    
+    /*******************************************************************************************************************************/
+	/*用于解码处HTML中的正确内容*/
+	//Element.innerHTML 属性设置或获取HTML语法表示的元素的后代。如果一个 <div>, <span>, 或 <noembed> 节点有一个文本子节点，该节点包含字符 (&), (<),  或(>), innerHTML 将这些字符分别返回为&amp;, &lt; 和 &gt; 。所以不能够用innerHTML直接获得文本内容。
+	//使用Node.textContent  可获取一个这些文本节点内容的正确副本。
     htmlDecode:function (text){
 		//创建div元素temp
         var temp = document.createElement("div");
@@ -114,105 +118,121 @@ var BlogDirectory = {
     interval 表示移动的速度
     */
     createBlogDirectory:function (id, mt, st, interval){
-        //获取博文正文div容器
+        //获取博文正文div容器elem
         var elem = document.getElementById(id);
-		//获取博文正文div容器失败则返回
+		//获取博文正文div容器失败（没有找到）则返回
         if(!elem) return false;
-        //获取正文div中的所有元素节点
-        var nodes = elem.getElementsByTagName("*");
-        //创建整个博客目录的div容器divSideBar
+        /*获取正文div中的所有元素节点的列表nodes*/
+		//getElementsByTagName() 方法返回元素的顺序是它们在文档中的顺序。如果把特殊字符串 "*" 传递给getElementsByTagName()方法，它将返回文档中所有元素的列表，元素排列的顺序就是它们在文档中的顺序。
+        //Element.getElementsByTagName() 方法返回一个动态的包含所有指定标签名的元素的HTML集合HTMLCollection。指定的元素的子树会被搜索，不包括元素自己。
+		//返回的列表是动态的，这意味着它会随着DOM树的变化自动更新自身。所以，使用相同元素和相同参数时，没有必要多次的调用Element.getElementsByTagName() .
+		//HTMLCollection 接口表示一个包含了元素（元素顺序为文档流中的顺序）的通用集合（generic collection），还提供了用来从该集合中选择元素的方法和属性。
+		var nodes = elem.getElementsByTagName("*");
+		/*******************************************************************************************************************************/
+        /*创建及设置整个博客目录的div容器*/
+		//创建整个博客目录（包含目录按钮和目录内容）的div容器divSideBar
         var divSideBar = document.createElement('DIV');
 		//设置博客目录div容器divSideBar的className为uprightsideBar
         divSideBar.className = 'uprightsideBar';
 		//设置博客目录div容器divSideBar的id为uprightsideBar
         divSideBar.setAttribute('id', 'uprightsideBar');
-		//创建divSideBar下即将包含一个<h2>标签的div容器divSideBarTab
+		/*******************************************************************************************************************************/
+		/*创建及设置目录按钮的div容器*/
+		//创建div容器divSideBarTab
         var divSideBarTab = document.createElement('DIV');
 		//将divSideBarTap的id设置为sideBarTab
         divSideBarTab.setAttribute('id', 'sideBarTab');
-		//将divSideBarTap加入博客目录div容器divSideBar的子元素列表
+		//将divSideBarTap设置为博客目录div容器divSideBar的子元素
         divSideBar.appendChild(divSideBarTab);
-		//创建一个即将包含在divSideBarTap中的<h2>元素
+		//创建一个即将包含在divSideBarTab（目录按钮的div容器）中的<h2>元素h2
         var h2 = document.createElement('H2');
-		//将上面创建的<h2>元素设置为divSideBarTab的子元素
+		//将上面创建的<h2>元素h2设置为divSideBarTab的子元素
         divSideBarTab.appendChild(h2);
 		//创建文本节点txt用于显示“目录”
         var txt = document.createTextNode('目录');
-		//将文本节点txt添加到<h2>内部
+		//将文本节点txt添加到<h2>内部（将txt设置为h2的子元素）
         h2.appendChild(txt);
+		/*******************************************************************************************************************************/
+		/*创建及设置目录内容的div容器*/
 		//创建目录内容的div容器divSideBarContents
         var divSideBarContents = document.createElement('DIV');
-		//容器divSideBarContents设置为不展示
+		//将目录内容的div容器divSideBarContents设置为不展示
         divSideBarContents.style.display = 'none';
 		//将div容器divSideBarContents的id设置为sideBarContents
         divSideBarContents.setAttribute('id', 'sideBarContents');
-		//将div容器添加到最外围的div容器divSideBar中
+		//将div容器添加到整个目录的div容器divSideBar中
         divSideBar.appendChild(divSideBarContents);
-        //创建自定义列表dlist（目录内容的外围容器，一个<d1>标签）
-        var dlist = document.createElement("dl");//将dlist（目录内容容器）设置为divSideBarContents（目录内容的div容器）的子元素
+		/*******************************************************************************************************************************/
+		/*创建及设置目录内容的div容器divSideBarContents下用于包含所有标题的<d1>标签*/
+        //创建dlist（用于包含所有标题的外围容器，一个<d1>标签）
+        var dlist = document.createElement("dl");
+		//将dlist（所有标题的容器）设置为divSideBarContents（目录内容的div容器）的子元素
         divSideBarContents.appendChild(dlist);
-        var num = 0;//统计找到的mt和st（统计<h2><h3>标题数）
+		//创建num统计找到第几个mt和st（<h2>，<h3>标签数）用于标题锚点
+        var num = 0;
         mt = mt.toUpperCase();//转化成大写方便创建元素
         st = st.toUpperCase();//转化成大写方便创建元素
-        //遍历正文div elem中的所有元素节点
+		/*******************************************************************************************************************************/
+        /*遍历正文div容器elem中的所有元素的HTMLCollection列表nodes并生成标题*/
         for(var i=0; i<nodes.length; i++)
         {
-			//判断节点名是不是H2或H3（是不是我们所要找的标题）
+			//判断元素名是不是H2或H3（是不是我们所要找的标题）
             if(nodes[i].nodeName == mt|| nodes[i].nodeName == st)    
             {
-                //将标签替换为空字符，获取标题内的文本
+                //将元素的标签符号替换为空字符，获取标题内的文本，g表示全局替换
                 var nodetext = nodes[i].innerHTML.replace(/<\/?[^>]+>/g,"");
-				//不区分大小写模式全局替换掉" "（空格）
+				//不区分大小写模式全局替换掉" "（空格），i表示不区分大小写
                 nodetext = nodetext.replace(/ /ig, "");
-				//对得到的nodetext进行处理
+				//对得到的nodetext进行处理（防止nodetext中的<>等符号变为HTML语法形式，使得标题内容出错），处理的结果为具体的标题文字
                 nodetext = BlogDirectory.htmlDecode(nodetext);
-                //插入锚，可就是给正文标题处(<h2>或<h3>)设置id        
+                //插入锚，也就是给正文标题处(<h2>或<h3>标签处)设置id
                 nodes[i].setAttribute("id", "blogTitle" + num);
-				//创建作为目录实际内容的文本对象
+				//创建用于包含单个标题文字的标签，这些标签内的内容将被设置为标题文字，所有这样的标签都将会包含在所有标题的容器dlist中
                 var item;
-				//判断标题节点种类(<h2>或<h3>，也就是H2或H3)
+				//判断标题种类(<h2>或<h3>，也就是H2或H3)
                 switch(nodes[i].nodeName)
                 {
-                    case mt:    //若为主标题
-						//创建标签为<dt>的元素
+                    case mt:    //若为主标题（h2）
+						//创建<dt>标签用于包含主标题标题文字
                         item = document.createElement("dt");
                         break;
-                    case st:    //若为次标题
-						//创建标签为<dd>的元素
+                    case st:    //若为次标题（h3）
+						//创建<dd>标签用于包含次标题标题文字
                         item = document.createElement("dd");
                         break;
                 }
-                
-				//根据nodetext创建文本节点itemtext
+				//根据标题文字nodetext创建文本节点itemtext
                 var itemtext = document.createTextNode(nodetext);
-				//将文本节点itemtext添加到item中
+				//将文本节点itemtext添加到用于包含单个标题文字的标签item中
                 item.appendChild(itemtext);
-				//设置目录内容（item）名如name="0"
+				//设置包含单个标题文字的标签item的名字，如name="0"
                 item.setAttribute("name", num);
 				//设置item被点击后的动作（创建锚链接效果）
-                item.onclick = function(){        //添加鼠标点击触发函数
+                item.onclick = function(){//鼠标点击触发函数
 					//通过item中的name值获得对应标题元素的位置
                     var pos = BlogDirectory.getElementPosition(document.getElementById("blogTitle" + this.getAttribute("name")));
-					//传入标题元素到顶部的距离进行窗口滚动，滚动失败则返回false
+					//传入标题元素到顶部的距离调用BlogDirectory.moveScrollBar函数进行窗口滚动，滚动失败则返回false
                     if(!BlogDirectory.moveScrollBar(pos.top, interval)) return false;
                 };
-                //将自定义表项（目录内容item）加入自定义列表（目录外部容器dlist）中（将目录的实际内容添加到目录内容的外围容器中）
+                //将包含单个标题文字的标签item加入用于包含所有标题的容器dlist中
                 dlist.appendChild(item);
+				//每生成并设置好一个标题，num加1用于下一个标题的锚点
                 num++;
             }
+			//if(nodes[i].nodeName == mt|| nodes[i].nodeName == st)结束
         }
+		//for(var i=0; i<nodes.length; i++)结束
         
 		//如果没有标题则返回false
         if(num == 0) return false; 
-
+		/*******************************************************************************************************************************/
         /*鼠标进入divSideBarTap时的事件处理*/
         var timerId1;
         divSideBarTab.onmouseenter = function(){
+			//用于判断操作是否发生于PC端，是则返回true，否则返回false
 			function IsPC() {
 				var userAgentInfo = navigator.userAgent;
-				var Agents = ["Android", "iPhone",
-                "SymbianOS", "Windows Phone",
-                "iPad", "iPod"];
+				var Agents = ["Android", "iPhone", "Windows Phone","iPad", "iPod"];
 				var flag = true;
 				for (var v = 0; v < Agents.length; v++) {
 					if (userAgentInfo.indexOf(Agents[v]) > 0) {
@@ -222,10 +242,12 @@ var BlogDirectory = {
 				}
 				return flag;
 			}
-
-var isPC = IsPC();
-
-			if(isPC){
+			//function IsPC()结束
+		
+			//调用函数判断是否为PC端
+			var isPC = IsPC();
+			
+			if(isPC){//如果是PC端
 				/*显示目录内容*/
 				divSideBarContents.style.display = "block";
 				/*隐藏目录按钮*/
@@ -239,11 +261,12 @@ var isPC = IsPC();
 				divSideBarContents.style.opacity=1;
 			}
         }
-//鼠标进入divSideBar时的动作
+		/*******************************************************************************************************************************/
+		//鼠标进入divSideBar时的动作，判断是否在计时，是则取消。用于在鼠标离开divSideBar 1秒内挽回目录内容，使其不1秒定时消失
 		divSideBar.onmouseenter = function(){
-			if(timerId1){clearTimeout(timerId1);}
+				if(timerId1){clearTimeout(timerId1);}
 		}
-
+		/*******************************************************************************************************************************/
         /*鼠标离开时的事件处理*/
         divSideBar.onmouseleave = function() {
 				timerId1 = setTimeout(function(){
@@ -253,11 +276,13 @@ var isPC = IsPC();
 							divSideBarTab.style.display = 'block';
 				},1000);
 		}
+		/*******************************************************************************************************************************/
 		//将整个博客目录的div容器divSideBar嵌入HTML页面中
         document.body.appendChild(divSideBar);
     }
-    
+    //createBlogDirectory()结束
 };
+//BlogDirectory结束
 
 window.onload=function(){
     /*页面加载完成之后生成博客目录*/
